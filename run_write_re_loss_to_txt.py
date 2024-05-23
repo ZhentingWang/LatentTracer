10ce5b0c17d206091151cb97feec71ea2e97a005
ZhentingWang <30305927+ZhentingWang@users.noreply.github.com>
2024-05-23 09:37:30 -0700

    update

--- run_write_re_loss_to_txt.py
+++ run_write_re_loss_to_txt.py
@@ -30,10 +30,10 @@ for name in name_list:
     print("input_content:",input_content)
 
     if args.use_random_init:
-        cmd = 'CUDA_VISIBLE_DEVICES={} python ddim_multi_sd_multilatent.py --model_type {} --input_selection_name {} --write_txt_path {} \
+        cmd = 'CUDA_VISIBLE_DEVICES={} python latenttracer.py --model_type {} --input_selection_name {} --write_txt_path {} \
             --distance_metric l2 --bs 1 --num_iter {} --strategy min --lr {} --use_random_init'.format(args.gpu,args.model_type,input_content,args.write_txt_path,args.num_iter,args.lr)
     else:
-        cmd = 'CUDA_VISIBLE_DEVICES={} python ddim_multi_sd_multilatent.py --model_type {} --input_selection_name {} --write_txt_path {} \
+        cmd = 'CUDA_VISIBLE_DEVICES={} python latenttracer.py --model_type {} --input_selection_name {} --write_txt_path {} \
             --distance_metric l2 --bs 1 --num_iter {} --strategy min --lr {}'.format(args.gpu,args.model_type,input_content,args.write_txt_path,args.num_iter,args.lr)
     counter = counter + 1
 
